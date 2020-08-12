1082aed4a71cd00973476d2abbacda55aec8bbdc
meenachinmay <chinmayanand896@gmail.com>
2020-08-12 20:00:50 +0530

    mongodb is connected

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 
+const mongoose = require('mongoose');
+
 const typesDef = gql`
     type Query {
         sayHi: String!
@@ -18,6 +20,10 @@ const resolvers = {
 const server = new ApolloServer({
     typeDefs: typesDef,
     resolvers: resolvers
+})
+
+mongoose.connect('mongodb://localhost:27017/react-graphql', {} , () => {
+    console.log('Mongodb is connected...');
 })
 
 server.listen({port: 5000})
