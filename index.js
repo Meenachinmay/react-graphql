026b09a57828afe65d6f31762f36e047c674ad99
meenachinmay <chinmayanand896@gmail.com>
2020-08-12 20:33:34 +0530

    first query done on graphql, success

--- index.js
+++ index.js
@@ -2,17 +2,36 @@ const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 
 const mongoose = require('mongoose');
+const config = require('./config');
+
+const User = require('./models/User');
+const Post = require('./models/Post');
 
 const typesDef = gql`
+
+    type Post {
+        id: ID!
+        body: String!
+        createdAt: String!
+        username: String!
+    }
+
     type Query {
-        sayHi: String!
+        getPosts: [Post]
     }
 `
 
 // process the logic
 const resolvers = {
     Query: {
-        sayHi: () => 'Hello world'
+        async getPosts() {
+            try {
+                const posts = Post.find();
+                return posts;
+            } catch (error) {
+                throw new Error(error);
+            }
+        }
     }
 }
 
@@ -22,9 +41,11 @@ const server = new ApolloServer({
     resolvers: resolvers
 })
 
-mongoose.connect('mongodb://localhost:27017/react-graphql', {} , () => {
+// @ connect to database
+mongoose.connect(config.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true } , () => {
     console.log('Mongodb is connected...');
 })
+
 
 server.listen({port: 5000})
     .then(res => {
